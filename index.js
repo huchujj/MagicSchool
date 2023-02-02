ab25f769936bf08c7b9ef8f5805f13a99302acd3
huchujj <tjddk6662@naver.com>
2023-02-03 01:54:09 +0900

    docs : Change "Press here!" to "Click"
    Issue : #1

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function AppearCircle() {
     let circle = document.getElementById("circle");
-    let PressHere = document.getElementById("PressHere");
+    let Click = document.getElementById("Click");
     let circleAppearSound = new Audio();
     circleAppearSound.src = "audio/AppearCircle.mp3";
 
-    PressHere.style.display = "none";
+    Click.style.display = "none";
     circle.style.display = "block";
     circle.style.animationPlayState = "running";
     circleAppearSound.play();
